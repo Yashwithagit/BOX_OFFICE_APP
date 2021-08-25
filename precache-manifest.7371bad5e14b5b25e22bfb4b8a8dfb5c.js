c3cf888f740f19f9aae99a7602568270eced8c0e
Yashwithagit <yashwithakrishnappa12@gmail.com>
2021-08-25 16:29:28 +0530

    Updates

---
+++ precache-manifest.7371bad5e14b5b25e22bfb4b8a8dfb5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c17d09698720cd1fd3c9ea10c6d83cb",
+    "url": "/BOX_OFFICE_APP/index.html"
+  },
+  {
+    "revision": "f2c2cf226edc8f1f0f58",
+    "url": "/BOX_OFFICE_APP/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "e2b221a825ebc1d44cae",
+    "url": "/BOX_OFFICE_APP/static/js/2.b0bbba38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BOX_OFFICE_APP/static/js/2.b0bbba38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c2cf226edc8f1f0f58",
+    "url": "/BOX_OFFICE_APP/static/js/main.3e5f3d40.chunk.js"
+  },
+  {
+    "revision": "c85dd48e400889cdc619",
+    "url": "/BOX_OFFICE_APP/static/js/runtime-main.60783b80.js"
+  }
+]);
